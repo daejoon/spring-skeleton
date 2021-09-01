abc9587079526c7674d9506b0dfbe10fa3340520
daejoon <kkode1911@gmail.com>
2021-09-02 00:35:45 +0900

    Added Spring Boot Batch Module

---
+++ skeleton-batch/src/test/java/com/ddoong2/batch/BatchAppTest.java
@@ -0,0 +1,15 @@
+package com.ddoong2.batch;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchAppTest {
+
+    @Test
+    @DisplayName("배치 환경을 로드 합니다")
+    void 배치_환경을_로드_합니다() throws Exception {
+    }
+
+}
